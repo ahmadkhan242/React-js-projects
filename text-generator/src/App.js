c744abc44570f37a948236e45867ebefb180c7c3
Mohammad Ahmad <md.ahmad0652@gmail.com>
2018-10-26 23:52:00 +0530

    new project text- generator is added

---
+++ text-generator/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props)
+  }
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
