eab6464bc906616283a0b379ae8709c37d35ee91
Mohammad Ahmad <md.ahmad0652@gmail.com>
2018-10-27 23:47:31 +0530

    text generator complete

--- text-generator/src/App.js
+++ text-generator/src/App.js
@@ -1,15 +1,61 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import './App.css';
+import Output from './Components/Output';
+import Select from './Components/controls/Select';
+import Text from './Components/controls/Text';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props){
-    super(props)
+    super(props);
+    this.state = {
+      paras: 4,
+      html: true,
+      text: ''
+    }
   }
+
+  componentWillMount(){
+    this.getSampleText();
+  }
+
+  getSampleText(){
+    axios.get('http://hipsterjesus.com/api?paras='+this.state.paras+'&html='+this.state.html)
+      .then((response) => {
+        this.setState({text: response.data.text}, function(){
+          console.log(this.state);
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  showHtml(x){
+      this.setState({html: x}, this.getSampleText);
+  }
+
+  changeParas(number){
+      this.setState({paras: number}, this.getSampleText);
+  }
+
   render() {
     return (
-      <div className="App">
-        
+      <div className="App container">
+        <h1 className="text-center">Text Generator</h1>
+        <hr />
+        <form className="form-inline">
+          <div className="form-group">
+            <label>Paragraphs:</label>
+            <Text value={this.state.paras} onChange={this.changeParas.bind(this)} />
+          </div>
+          <div className="form-group">
+            <label>Include HTML:</label>
+            <Select value={this.state.html} onChange={this.showHtml.bind(this)} />
+          </div>
+        </form>
+        <br /><br />
+        <Output value={this.state.text} />
       </div>
     );
   }
